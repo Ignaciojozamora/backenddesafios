b4f64511ed85aa404b8fce41f0a624a321c999b9
Ignacio Zamora <ignaciojo.zamora@gmail.com>
2023-04-10 21:52:51 -0300

    desafio2

--- desafio1.js
+++ desafio1.js
@@ -58,14 +58,14 @@ const product1 = new Product("Arroz", "Arroz", 150, "Sin Imagen", "A123", 20)
 const product2 = new Product("Fideos", "Fideos", 250, "Sin Imagen", "F123", 10)
 const product3 = new Product("Azucar", "Azucar", 320, "Sin Imagen", "A456", 30)
 const product4 = new Product("Te", "Te", 120, "Sin Imagen", "T123", 40)
-const product5 = new Product()
+
 
 const productManager = new ProductManager()
 productManager.addProduct(product1)
 productManager.addProduct(product2)
 productManager.addProduct(product3)
 productManager.addProduct(product4)
-productManager.addProduct(product5)
+
 console.log("Agregado de producto 1 ya existente .............",productManager.addProduct(product1))
 console.log("Producto ID 2.............",productManager.getProductById(2))
 console.log("Producto ID 5.............",productManager.getProductById(5))
